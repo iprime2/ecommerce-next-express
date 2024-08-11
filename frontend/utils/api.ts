b1e26ca53f75801fcfc8812fbd80fc3a9c73ace1
Ubuntu <ubuntu@ip-172-31-34-48.ap-south-1.compute.internal>
2024-08-11 16:36:07 +0000

    ec2 changes

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -3,7 +3,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 // Load the base URL from the environment variables
-// const BASE_URL = process.env.LOCALHOST || 'http://localhost:5000';
+//const BASE_URL = process.env.LOCALHOST || 'http://localhost:5000';
 const BASE_URL = process.env.LOCALHOST || 'http://ec2-13-235-104-169.ap-south-1.compute.amazonaws.com:5000';
 
 // Create an Axios instance with the base URL
