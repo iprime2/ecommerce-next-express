17541dd6b9814c2f485b1d24651f22b9ec828f79
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2024-08-11 17:32:08 +0530

    cros fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,13 @@ app.use(morgan('combined'));
 app.use(helmet());
 
 // CORS: Enables Cross-Origin Resource Sharing.
-// app.use(cors());
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(sessionMiddleware);
 
