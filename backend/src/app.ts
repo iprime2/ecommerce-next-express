67b1ae0650da61fc6f92af7a2a938960efcccc79
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2024-11-17 15:42:54 +0530

    cors fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,13 +27,6 @@ app.use(cors({
     credentials: true // Allow credentials if needed
 }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(sessionMiddleware);
 
 app.use(bodyParser.json());
